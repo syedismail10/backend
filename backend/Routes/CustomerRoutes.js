246eeaf7a2f35e3bcace6f03535f7ec3116c2ada
syedismail10 <syedismailulhaq@gmail.com>
2023-12-04 20:05:31 +0500

    tro

---
+++ backend/Routes/CustomerRoutes.js
@@ -0,0 +1,10 @@
+import express from "express";
+import db from '../db/dbConnection.js'
+
+const customerRoute = express.Router()
+
+customerRoute.post('/register',async (req, res)=> {
+
+})
+
+export default customerRoute
